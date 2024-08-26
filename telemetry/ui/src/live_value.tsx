c2afc57d56965ae213ab5e0ac0b00da315b84bbf
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-08-26 22:59:50 +1000

    add determining color logic

--- telemetry/ui/src/live_value.tsx
+++ telemetry/ui/src/live_value.tsx
@@ -5,10 +5,20 @@ interface TemperatureProps {
 }
 
 function LiveValue({ temp }: TemperatureProps) {
-  let valueColour = "white";
-
+  const determineColor = () => {
+    if (temp < 20 || temp > 80) {
+      return "#FF3348";
+    } else {
+      if ((20 <= temp && temp <= 25) ||
+          (75 <= temp && temp <= 80)) {
+        return "yellow";
+      } else {
+        return "#33FF00";
+      }
+    }
+  }
   return (
-    <header className="live-value" style={{ color: valueColour }}>
+    <header className="live-value" style={{ color: determineColor() }}>
       {`${temp.toPrecision(3)}°C`}
     </header>
   );
